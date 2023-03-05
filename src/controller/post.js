c2815dc7145821c01a369d9a1365cd585f9dbfce
ViTrans <tran.vi487@gmail.com>
2023-03-05 09:43:04 +0700

    upload fix bug comments

--- src/controller/post.js
+++ src/controller/post.js
@@ -1,19 +1,22 @@
-const Category = require('../models/category');
-const Post = require('../models/posts');
-const path = require('path');
-const url = require('url');
+const Category = require("../models/category");
+const Post = require("../models/posts");
+const path = require("path");
+const url = require("url");
 
 // form
 const newForm = async (req, res) => {
   try {
     // categoryId;
-    const categories = await Category.find({}).select('_id name');
+    const categories = await Category.find({}).select("_id name");
     console.log(categories);
-    res.render(path.join(__dirname, '..', 'views', 'admin', 'posts', 'create'), {
-      categories,
-      title: 'Thêm bài viết',
-      // post : new Post()
-    });
+    res.render(
+      path.join(__dirname, "..", "views", "admin", "posts", "create"),
+      {
+        categories,
+        title: "Thêm bài viết",
+        post: new Post(),
+      }
+    );
   } catch (error) {
     console.log(error);
   }
@@ -23,73 +26,81 @@ const editForm = async (req, res) => {
   try {
     console.log(req.params.id);
     const category = await Category.findById(req.params.id);
-    res.render(path.join(__dirname, '..', 'views', 'admin', 'categories', 'edit'), {
-      title: 'Sử danh mục',
-      category: category,
-    });
+    res.render(
+      path.join(__dirname, "..", "views", "admin", "categories", "edit"),
+      {
+        title: "Sử danh mục",
+        category: category,
+      }
+    );
   } catch (error) {
-    console.log('loi tai editformed', error);
-    res.redirect('/admin/categories');
+    console.log("loi tai editformed", error);
+    res.redirect("/admin/categories");
   }
 };
-
 
 const remove = async (req, res) => {
   try {
     await Category.findByIdAndDelete(req.body.id);
-    res.redirect('/admin/categories');
+    res.redirect("/admin/categories");
   } catch (error) {
-    res.redirect('/admin/categories');
+    res.redirect("/admin/categories");
   }
 };
 
 const create = async (req, res, next) => {
   console.log(req.body);
-  const categories = await Category.find({}).select('_id name');
+  const categories = await Category.find({}).select("_id name");
 
   const newPost = {
     categoryId: req.body?.categoryId,
     title: req.body?.title,
     content: req.body?.content,
-    feature: req.body?.feature == '1' ? true : false,
-    image: req?.file,
+    feature: req.body?.feature == "1" ? true : false,
+    image: req?.file.path,
   };
   const post = new Post(newPost);
   try {
     await post.save();
-    res.redirect('/admin/posts');
+    res.redirect("/admin/posts");
   } catch (error) {
     console.log(error);
-    res.render(path.join(__dirname, '..', 'views', 'admin', 'posts', 'create'), {
-      title: 'posts',
-      post: newPost,
-      categories,
-    });
+    res.render(
+      path.join(__dirname, "..", "views", "admin", "posts", "create"),
+      {
+        title: "posts",
+        post: newPost,
+        categories,
+      }
+    );
   }
 };
 
 const edit = async (req, res, next) => {
   console.log(req.body);
-  const categories = await Category.find().select('_id name');
+  const categories = await Category.find().select("_id name");
 
   const newPost = {
     categoryId: req.body?.categoryId,
     title: req.body?.title,
     content: req.body?.content,
-    feature: req.body?.feature == '1' ? true : false,
+    feature: req.body?.feature == "1" ? true : false,
     image: req?.file,
   };
   const post = new Post(newPost);
   try {
     await post.save();
-    res.redirect('/admin/posts');
+    res.redirect("/admin/posts");
   } catch (error) {
     console.log(error);
-    res.render(path.join(__dirname, '..', 'views', 'admin', 'posts', 'create'), {
-      title: 'posts',
-      post: newPost,
-      categories,
-    });
+    res.render(
+      path.join(__dirname, "..", "views", "admin", "posts", "create"),
+      {
+        title: "posts",
+        post: newPost,
+        categories,
+      }
+    );
   }
 };
 module.exports = {
